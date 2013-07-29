3daca8b884efc50439010f42b2965caea0b6afbf
str <str@localhost>
2013-07-29 13:51:21 +0200

    adding code for scientific notation

---
+++ testConsole_dotNET/MathTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using testConsole_dotNET.Logging;
+
+namespace testConsole_dotNET
+{
+    class MathTests
+    {
+        internal void TestScientificNotation()
+        {
+            Output.LogTestStart("TestScientificNotation");
+
+            double tenToThePowerOfMinusFifteen = 1e-15;
+
+            Output.Log("tenToThePowerOfMinusFifteen = " + tenToThePowerOfMinusFifteen);
+        }
+    }
+}
