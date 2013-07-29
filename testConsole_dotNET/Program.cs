e8692201960d9bf14f62e26f07999cb3e4f13d33
str <str@localhost>
2013-07-29 10:15:11 +0200

    adding test for .NET inheritance

--- testConsole_dotNET/Program.cs
+++ testConsole_dotNET/Program.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace testConsole_dotNET
 {
-   class Program
-   {
-      static void Main(string[] args)
-      {
-         Test test = new Test();
-         test.Go();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.Go();
 
-         Console.WriteLine("Press ENTER to finish");
-         Console.ReadLine();
-      }
-   }
+            Console.WriteLine("Press ENTER to finish");
+            Console.ReadLine();
+        }
+    }
 }
